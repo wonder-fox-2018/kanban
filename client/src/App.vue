aab81d20fe7fca8cbdb8a26581efa0f32e0a70b6
esadeli <efrat.sadeli@gmail.com>
2018-10-17 12:14:24 +0700

    Add Data OK

---
+++ client/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <div class="contentCustom">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Plan</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Start</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">On Progress</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Finish</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+    -->
+  </div>
+</template>
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+#contentCustom {
+  margin-top: 400px;
+}
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Personal Kanban</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+      <button class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#addTaskModal" >Add Task</button>
+    </nav>
+    <!-- Modal Part -->
+    <!-- Modal -->
+    <div class="modal fade" id="addTaskModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Add Task</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+             <label for="exampleInputEmail1">Title</label>
+             <input v-model="tasktitle" type="text" class="form-control" aria-describedby="emailHelp" placeholder="Enter Title">
+             <label for="exampleInputEmail1">Description</label>
+             <input v-model="taskdescription" type="text" class="form-control" aria-describedby="emailHelp" placeholder="Enter Description">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-success" v-on:click="addTask()" >Add Task</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import db from '../../googlekey.js'
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      tasktitle: '',
+      taskdescription: '',
+      taskstatus: 'PLAN'
+    }
+  },
+  methods: {
+    addTask () {
+      // console.log('add task')
+      db.ref('/task/').push({
+        title: this.tasktitle,
+        description: this.taskdescription,
+        status: this.taskstatus
+      }, (error) => {
+        if (!error) {
+          // console.log('okay----')
+          // eslint-disable-next-line
+          $('#addTaskModal').modal('hide')
+        } else {
+          console.log('ERROR Add Task to Firebase: ', error)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
