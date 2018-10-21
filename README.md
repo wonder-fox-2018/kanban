854f4719b3c84d240cc593d3333db1fe4188ce2a
agungatd <agungatidhira@gmail.com>
2018-10-21 11:19:33 +0700

    done with basic feature

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
-# kanban
+# test-client
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
-### Run your tests
-```
-yarn run test
-```
-
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
+
+### Link Firebase
+
+https://kanban-74e11.firebaseapp.com/#/
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  created () {},
+  data: function () {
+    return {}
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,6 +20,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
