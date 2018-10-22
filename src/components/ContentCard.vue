14cdcc5536ed673203cb6a970fe2aa36a8b03263
adishare <adie.share@gmail.com>
2018-10-22 12:29:35 +0700

    kanban

---
+++ src/components/ContentCard.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="cardContent">
+    <h2>{{item.title}}</h2>
+    <div class="container-button">
+      <div>
+        <h4 @click="actionOne">{{buttonOne}}</h4>
+      </div>
+      <div>
+        <h4 @click="actionTwo" v-if="buttonTwo">{{buttonTwo}}</h4>
+      </div>
+      <div>
+        <h4 @click="removeItem">delete</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import database from '../assets/config.js'
+  export default {
+    name: 'ContentCard',
+    data: function() {
+      return {
+        buttonOne: '',
+        buttonTwo: ''
+      }
+    },
+    props: ['item', 'name'],
+    methods: {
+      actionOne() {
+        database.ref(`/${this.item.id}`).remove()
+        database.ref('/').push({
+          title: this.item.title,
+          status: this.buttonOne
+        })
+      },
+      actionTwo() {
+        database.ref(`/${this.item.id}`).remove()
+        database.ref('/').push({
+          title: this.item.title,
+          status: this.buttonTwo
+        })
+      },
+      removeItem() {
+        database.ref(`/${this.item.id}`).remove()
+      }
+    },
+    created() {
+      if (this.name === 'Pre-Log') {
+        this.buttonOne = 'To-Do'
+        this.buttonTwo = null
+      } else if (this.name === 'To-Do') {
+        this.buttonOne = 'Pre-Log'
+        this.buttonTwo = 'On-Going'
+      } else if (this.name === 'On-Going') {
+        this.buttonOne = 'Finished'
+        this.buttonTwo = 'To-Do'
+      } else if (this.name === 'Finished') {
+        this.buttonOne = 'On-Going'
+        this.buttonTwo = null
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .cardContent {
+    font-size: 10px;
+    color: #fff;
+    width: 70%;
+    min-height: 100px;
+    background-color: #5f7656;
+    box-shadow: 0px 5px 4px #c0c0c09d;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    margin-bottom: 50px;
+    vertical-align: middle;
+    cursor: pointer;
+    padding-bottom: 20px;
+  }
+  
+  .card h2 {
+    padding-top: 15px;
+    font-size: 20px;
+  }
+  
+  .container-button h4 {
+    color: #4684f6;
+    background-color: #fff;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    width: 30%;
+    padding: 2%;
+  }
+  
+  .container-button {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+</style>
---
+++ src/components/KanbanCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <h2>{{data.name}}</h2>
+    <ContentCard v-for="(item) in data.tasks" :key="item.id" :item="item" :name="data.name"></ContentCard>
+  </div>
+</template>
+
+<script>
+  import ContentCard from '@/components/ContentCard.vue'
+  export default {
+    name: 'KanbanCard',
+    components: {
+      ContentCard
+    },
+    data: function() {
+      return {}
+    },
+    props: ['data'],
+    created() {
+      this.cardTitle = this.title
+    }
+  }
+</script>
+
+<style scoped>
+  .card {
+    font-size: 10px;
+    color: #4684f6;
+    width: 100%;
+    min-height: 400px;
+    height: 100%;
+    background-color: white;
+    box-shadow: 0px 0px 5px 5px #c0c0c09d;
+  }
+  
+  .card h2 {
+    padding: 5px;
+  }
+</style>
